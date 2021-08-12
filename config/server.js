3ee8d4a0321504a5a4364d7588020b6dc26af94b
Novailoveyou <novailoveyoux@gmail.com>
2021-08-12 15:07:36 +0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2985e4cc4300d92c12a690347594e0ad'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'de0e068f-1a2b-4245-ac85-ff54b260b86d'
+};
