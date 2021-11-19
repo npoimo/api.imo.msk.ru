5214ec8b1fd6080f236bdfce3c0fecc3bd3bcd39
Novailoveyou <novailoveyoux@gmail.com>
2021-11-19 12:01:38 +0300

    envs update

--- config/database.js
+++ config/database.js
@@ -4,17 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'main.m55fm.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'mip-institute'),
-        username: env('DATABASE_USERNAME', 'nover'),
-        password: env('DATABASE_PASSWORD', 'GI1LEms0xicZ1HZp'),
+        host: env('DATABASE_HOST'),
+        srv: env.bool('DATABASE_SRV'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD')
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
-    },
-  },
-});
+        authenticationDatabase: env('AUTHENTICATION_DATABASE'),
+        ssl: env.bool('DATABASE_SSL')
+      }
+    }
+  }
+})
--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '2985e4cc4300d92c12a690347594e0ad'),
-    },
-  },
-});
+      secret: env('ADMIN_JWT_SECRET')
+    }
+  }
+})
